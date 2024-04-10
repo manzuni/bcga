6be070c5a49c69fd6ab65047350dc5d4d32e47e3
madalin.antonoiu <madalin.antonoiu@crunch.co.uk>
2024-04-10 09:30:55 +0100

    Ready to edit?

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+const content = ref({});
+
+const fetchContent = async () => {
+  const data = useContent();
+  content.value = data.page.value;
+};
+
+fetchContent();
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <h1>{{ content.title }}</h1>
+    <h2>{{ content.header }}</h2>
+  </div>
+</template>

f21547971e5090b728e2aaa6fb52587350c32a2c
madalin.antonoiu <madalin.antonoiu@crunch.co.uk>
2024-04-10 19:59:29 +0100

    Dooter

--- components/Footer.vue
+++ components/Footer.vue
@@ -105,7 +105,20 @@
     <!--Copyright section-->
     <div class="w-full bg-black/5 p-4 text-center">
       <div>© 2024: Biserica Creștină Golgota Aveley</div>
-      <div>Hall Avenue Aveley, South Ockendon, RM15 4LD, UK</div>
+      <div class="flex justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24px"
+          height="24px"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7"
+          />
+        </svg>
+        Hall Avenue Aveley, South Ockendon, RM15 4LD, UK
+      </div>
     </div>
   </footer>
 </template>
